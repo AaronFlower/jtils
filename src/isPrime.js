a31f9c2b75b799010d8d90c23fb0087ef00bbb25
AaronFlower <haojunzhan@gmail.com>
2019-01-15 11:36:43 +0800

    use sqrt to optimize

--- src/isPrime.js
+++ src/isPrime.js
@@ -1,6 +1,6 @@
 const isPrime = n => {
   if (!Number.isInteger(n) || n < 2) return false
-  for (let i = 2; i < n; i++) {
+  for (let i = 2, r = Math.sqrt(n); i <= r; i++) {
     if (n % i === 0) {
       return false
     }
--- test/isPrime.js
+++ test/isPrime.js
@@ -18,7 +18,7 @@ describe('isPrime', () => {
   it('should handle "integer number" correctly', () => {
     [2, 3, 5, 7, 11, 13, 17].map(v => assert.equal(isPrime(v), true))
     assert.equal(isPrime(0), false)
-    Array.from([4, 6, 8, 12, 15]).map(v => assert.equal(isPrime(v), false))
+    Array.from([4, 6, 8, 12, 15, 178]).map(v => assert.equal(isPrime(v), false))
     assert.equal(isPrime(1), false)
     assert.equal(isPrime(-2), false)
   })
